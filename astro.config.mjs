b61aa4e5f95c57853d583683e9034a83c8e7d6bf
Will Jackson <will@willjackson.uk>
2025-07-21 14:18:01 +0100

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://recipes.willjackson.uk',
+    integrations: [mdx(), sitemap(), icon()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,18 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const recipes = defineCollection({
+	// Load Markdown and MDX files in the `src/content/recipes/` directory.
+	loader: glob({ base: './src/content/recipes', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: image().optional(),
+	}),
+});
+
+export const collections = { blog , recipes};
